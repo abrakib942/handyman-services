96c88431cbb99613ad7618289ff3a926af1622e6
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-10-15 11:29:47 +0600

    user zod validation

---
+++ backend/src/app/modules/user/user.validation.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+const signUpUserZodSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'name is required',
+    }),
+    email: z.string({
+      required_error: 'email is required',
+    }),
+    password: z.string({
+      required_error: 'password is required',
+    }),
+    role: z.enum(['user', 'admin', 'super_admin']).optional(),
+  }),
+});
+
+const loginUserZodSchema = z.object({
+  body: z.object({
+    email: z.string({
+      required_error: 'email is required',
+    }),
+    password: z.string({
+      required_error: 'password is required',
+    }),
+  }),
+});
+
+export const UserValidation = {
+  signUpUserZodSchema,
+  loginUserZodSchema,
+};
